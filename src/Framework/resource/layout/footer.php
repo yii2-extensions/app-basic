a692cba7057925fe28a9affbd8f47754e6b4b02b
Wilmer Arambula <terabytesoftw@gmail.com>
2023-10-10 08:24:15 -0300

    More rework code.

--- src/Framework/resource/layout/footer.php
+++ src/Framework/resource/layout/footer.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 use sjaakp\icon\Icon;
 use yii\bootstrap5\Html;
+use yii\web\View;
 
+/**
+ * @var View $this
+ */
 ?>
 <?= Html::beginTag('div', ['class' => 'container']) ?>
     <?= Html::beginTag('footer', ['class' => 'd-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top']) ?>
@@ -63,44 +67,7 @@ use yii\bootstrap5\Html;
                 ),
                 ['class' => 'ms-3'],
             ) ?>
-            <li class="ms-1">
-                <!-- Inicio Toggle modo dark -->
-                <div class="dropdown bd-mode-toggle">
-                <button class="btn btn-bd-primary dropdown-toggle d-flex align-items-center"
-                        id="bd-theme"
-                        type="button"
-                        aria-expanded="false"
-                        data-bs-toggle="dropdown"
-                        aria-label="Toggle theme (auto)">
-                    <?= Icon::renderIcon('solid', 'circle-half-stroke', ['class' => 'me-2 fa-solid fa-xl theme-icon-active']) ?>
-                    <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-                    <li>
-                    <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-                        <?= Icon::renderIcon('solid', 'sun', ['class' => 'me-2 fa-solid theme-icon']) ?>
-                        Light
-                        <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-                    </button>
-                    </li>
-                    <li>
-                    <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-                        <?= Icon::renderIcon('solid', 'moon', ['class' => 'me-2 fa-solid theme-icon']) ?>
-                        Dark
-                        <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-                    </button>
-                    </li>
-                    <li>
-                    <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-                        <?= Icon::renderIcon('solid', 'circle-half-stroke', ['class' => 'me-2 fa-solid theme-icon']) ?>
-                        Auto
-                        <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-                    </button>
-                    </li>
-                </ul>
-                </div>
-                <!-- Fin Toggle modo dark -->
-            </li>
+            <?= $this->render('component/toggle_dark') ?>
         <?= Html::endTag('ul') ?>
     <?= Html::endTag('footer') ?>
 <?= Html::endTag('div') ?>
