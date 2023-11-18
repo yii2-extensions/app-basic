9237fd4a0dab30efddebe6e961d7c281409a8cd7
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-11-18 09:15:35 -0300

    Improve language config. (#56)

--- src/Framework/resource/layout/component/toggle_language.php
+++ src/Framework/resource/layout/component/toggle_language.php
@@ -23,43 +23,43 @@ echo Dropdown::widget()
             ->iconClass('fi fi-cn fis me-2')
             ->link(Url::current(['language' => 'zh']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'zh'),
+            ->active(Yii::$app->language === 'zh-CN'),
         Item::create()
             ->label(Yii::t('app.basic', 'German'))
             ->iconClass('fi fi-de fis me-2')
             ->link(Url::current(['language' => 'de']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'de'),
+            ->active(Yii::$app->language === 'de-DE'),
         Item::create()
             ->label(Yii::t('app.basic', 'English'))
             ->iconClass('fi fi-us fis me-2')
             ->link(Url::current(['language' => 'en']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'en'),
+            ->active(Yii::$app->language === 'en-US'),
         Item::create()
             ->label(Yii::t('app.basic', 'French'))
             ->iconClass('fi fi-fr fis me-2')
             ->link(Url::current(['language' => 'fr']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'fr'),
+            ->active(Yii::$app->language === 'fr-FR'),
         Item::create()
             ->label(Yii::t('app.basic', 'Portuguese'))
             ->iconClass('fi fi-pt fis me-2')
             ->link(Url::current(['language' => 'pt']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'pt'),
+            ->active(Yii::$app->language === 'pt-BR'),
         Item::create()
             ->label(Yii::t('app.basic', 'Spanish'))
             ->iconClass('fi fi-es fis me-2')
             ->link(Url::current(['language' => 'es']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'es'),
+            ->active(Yii::$app->language === 'es-ES'),
         Item::create()
             ->label(Yii::t('app.basic', 'Russian'))
             ->iconClass('fi fi-ru fis me-2')
             ->link(Url::current(['language' => 'ru']))
             ->linkClass('dropdown-item d-flex align-items-center')
-            ->active(Yii::$app->language === 'ru'),
+            ->active(Yii::$app->language === 'ru-RU'),
     )
     ->listClass('dropdown-menu dropdown-menu-end shadow')
     ->toggleAttributes(
