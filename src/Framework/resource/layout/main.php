b43ba93e863cee9c99d2fa4c7fbea2283a60ba7b
Luiz Marin <67489841+luizcmarin@users.noreply.github.com>
2024-04-14 11:10:18 -0300

    Docs folder standardization and other fixes

--- src/Framework/resource/layout/main.php
+++ src/Framework/resource/layout/main.php
@@ -13,27 +13,26 @@ use yii\{bootstrap5\Breadcrumbs, web\View};
  */
 AppAsset::register($this);
 ?>
-<?php $this->beginPage() ?>
-    <!DOCTYPE html>
-    <?= Html::widget()->attributes(['lang' => Yii::$app->language, 'data-bs-theme' => 'auto'])->begin() ?>
-        <?= $this->render('head') ?>
-        <?php $this->beginBody() ?>
-            <?= Body::widget()->class('d-flex')->begin() ?>
-                <?= Div::widget()->class('cover-container d-flex w-100 h-100 mx-auto flex-column')->begin() ?>
-                    <?= Header::widget()->class('mb-auto')->begin() ?>
-                        <?= $this->render('component/menu') ?>
-                        <?= $this->render('component/alert') ?>
-                        <?= Breadcrumbs::widget(
-                            [
-                                'links' => $this->params['breadcrumbs'] ?? [],
-                            ]
-                        ) ?>
-                    <?= Header::end() ?>
-                    <?= $content ?>
-                    <?= $this->render('footer') ?>
-                <?= Div::end() ?>
-                <?= Icon::symbols($this) ?>
-            <?= Body::end() ?>
-        <?php $this->endBody() ?>
-    <?= Html::end() ?>
+<?php $this->beginPage() ?><!DOCTYPE html>
+<?= Html::widget()->attributes(['lang' => Yii::$app->language, 'data-bs-theme' => 'auto'])->begin() ?>
+<?= $this->render('head') ?>
+<?php $this->beginBody() ?>
+<?= Body::widget()->class('d-flex')->begin() ?>
+<?= Div::widget()->class('cover-container d-flex w-100 h-100 mx-auto flex-column')->begin() ?>
+<?= Header::widget()->class('mb-auto')->begin() ?>
+<?= $this->render('component/menu') ?>
+<?= $this->render('component/alert') ?>
+<?= Breadcrumbs::widget(
+    [
+        'links' => $this->params['breadcrumbs'] ?? [],
+    ]
+) ?>
+<?= Header::end() ?>
+<?= $content ?>
+<?= $this->render('footer') ?>
+<?= Div::end() ?>
+<?= Icon::symbols($this) ?>
+<?= Body::end() ?>
+<?php $this->endBody() ?>
+<?= Html::end() ?>
 <?php $this->endPage();
--- yii.bat
+++ yii.bat
@@ -5,7 +5,7 @@ rem  Yii command line bootstrap script for Windows.
 rem
 rem  @author Qiang Xue <qiang.xue@gmail.com>
 rem  @link https://www.yiiframework.com/
-rem  @copyright Copyright (c) 2008 Yii Software LLC
+rem  @copyright Copyright © 2008 by Yii Software (https://www.yiiframework.com/)
 rem  @license https://www.yiiframework.com/license/
 rem -------------------------------------------------------------
 
