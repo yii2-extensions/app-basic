fcd887b6ae163231e9176566148ec538fc7c8ac4
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-30 19:32:11 -0400

    fix: Correct command syntax in `build.yml` and add conditional loading for `c3.php` in `index.php`.

--- web/index.php
+++ web/index.php
@@ -12,6 +12,14 @@ define('YII_ENV', getenv('YII_ENV') ?? 'prod');
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+if (getenv('YII_C3')) {
+    $c3 = dirname(__DIR__) . '/c3.php';
+
+    if (file_exists($c3)) {
+        require_once $c3;
+    }
+}
+
 $config = require dirname(__DIR__) . '/config/web/app.php';
 
 $runner = new RoadRunner(new StatelessApplication($config));
