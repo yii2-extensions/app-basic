8f9ea24957a239b8b35e903b7a968b2791308da4
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-30 14:48:28 -0400

    fix: Update `i18n` configuration in components.php and correct document `root` in `php-builtin.yml`.

--- config/web/components.php
+++ config/web/components.php
@@ -2,12 +2,22 @@
 
 declare(strict_types=1);
 
+use yii\i18n\PhpMessageSource;
+
 /** @phpstan-var string[] $commonComponents */
 $commonComponents = require dirname(__DIR__) . '/common/components.php';
 
 $config = [
     'errorHandler' => [
         'errorAction' => 'site/404',
+    ],
+    'i18n' => [
+        'translations' => [
+            'app.basic' => [
+                'class' => PhpMessageSource::class,
+                'basePath' => '@resource/message',
+            ],
+        ],
     ],
     'request' => [
         'cookieValidationKey' => 'your-cookie-validation-key',
