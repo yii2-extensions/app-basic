02dc3a090e830fa630483c94eb5acf456e4c266a
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-30 19:13:12 -0400

    feature(app): Implement `RoadRunner` stack.

--- web/index.php
+++ web/index.php
@@ -2,26 +2,18 @@
 
 declare(strict_types=1);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->safeLoad();
+use yii2\extensions\psrbridge\http\StatelessApplication;
+use yii2\extensions\roadrunner\RoadRunner;
 
-define('YII_DEBUG', $_ENV['YII_DEBUG'] ?? false);
-define('YII_ENV', $_ENV['YII_ENV'] ?? 'prod');
+define('YII_DEBUG', getenv('YII_DEBUG') ?? false);
+define('YII_ENV', getenv('YII_ENV') ?? 'prod');
 
-require_once dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php';
-
-if (getenv('YII_C3')) {
-    $c3 = dirname(__DIR__) . '/c3.php';
-
-    if (file_exists($c3)) {
-        require_once $c3;
-    }
-}
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require dirname(__DIR__) . '/config/web/app.php';
 
-$app = new yii\web\Application($config);
-$app->run();
+$runner = new RoadRunner(new StatelessApplication($config));
+
+$runner->run();
