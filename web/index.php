2db6c7306761c496a40397f1b069f26d809c5f7f
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-30 20:27:24 -0400

    fix: Remove Codeception coverage configuration and related conditions from build files.

--- web/index.php
+++ web/index.php
@@ -14,14 +14,6 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require dirname(__DIR__) . '/config/web/app.php';
 
-if (getenv('YII_C3')) {
-    $c3 = dirname(__DIR__) . '/c3.php';
-
-    if (file_exists($c3)) {
-        require_once $c3;
-    }
-}
-
 $runner = new RoadRunner(new StatelessApplication($config));
 
 $runner->run();
