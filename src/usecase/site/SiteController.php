61558daaac309b8442a1ea84a92a7038f36d916f
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-30 20:11:38 -0400

    fix: Replace `ArrayHelper::merge` with native `array_merge` in SiteController.

--- src/usecase/site/SiteController.php
+++ src/usecase/site/SiteController.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace app\usecase\site;
 
 use app\usecase\Controller;
-use yii\helpers\ArrayHelper;
 use yii\web\ErrorAction;
+
+use function array_merge;
 
 final class SiteController extends Controller
 {
@@ -17,11 +18,11 @@ final class SiteController extends Controller
 
     public function actions(): array
     {
-        return ArrayHelper::merge(
+        return array_merge(
             [
-                '404' => ['class' => ErrorAction::class],
+              '404' => ['class' => ErrorAction::class],
             ],
-            parent::actions(),
+            parent::actions()
         );
     }
 
