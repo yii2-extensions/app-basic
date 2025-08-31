1b46ce986f2c710fd67451bbc14c853267084d61
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-31 06:42:40 -0400

    fix(app): Remove all stack configurations.

--- src/usecase/site/SiteController.php
+++ src/usecase/site/SiteController.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace app\usecase\site;
 
 use app\usecase\Controller;
-use yii\helpers\ArrayHelper;
 use yii\web\ErrorAction;
+
+use function array_merge;
 
 final class SiteController extends Controller
 {
@@ -17,7 +18,7 @@ final class SiteController extends Controller
 
     public function actions(): array
     {
-        return ArrayHelper::merge(
+        return array_merge(
             [
                 '404' => ['class' => ErrorAction::class],
             ],
