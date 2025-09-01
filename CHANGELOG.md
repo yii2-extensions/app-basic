7603459dd86afd7dcc3b0244936ec774fe2cf3e0
Wilmer Arambula <terabytesoftw@gmail.com>
2025-09-01 05:12:20 -0400

    fix(app): Add container configuration to `console` app setup.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 - Bug #143: Add `Available stacks` section with `RoadRunner` badge to `README.md` (@terabytesoftw)
 - Bug #146: Remove `yiisoft/yii2-symfonymailer` dependency from `composer.json` (@terabytesoftw)
 - Bug #149: Update PHPStan annotations and add container configuration file (@terabytesoftw)
+- Bug #150: Add container configuration to `console` app setup (@terabytesoftw)
 
 ## 0.1.0 August 31, 2025
 
--- config/console/app.php
+++ config/console/app.php
@@ -6,6 +6,8 @@ use app\usecase\hello\HelloController;
 
 /** @phpstan-var array<string, mixed> $components */
 $components = require dirname(__DIR__) . '/common/components.php';
+/** @phpstan-var array<string, mixed> $container */
+$container = require dirname(__DIR__) . '/console/container.php';
 /** @phpstan-var array<string, mixed> $params */
 $params = require dirname(__DIR__) . '/params-console.php';
 
@@ -14,6 +16,7 @@ return [
     'basePath' => dirname(__DIR__, 2),
     'bootstrap' => ['log'],
     'components' => $components,
+    'container' => $container,
     'controllerMap' => ['hello' => HelloController::class],
     'params' => $params,
 ];
