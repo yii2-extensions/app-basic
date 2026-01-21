c186a2a55097f53aca21436f69d841305a1da3c6
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2026-01-21 20:09:59 -0300

    fix(docs): Add copyright and license information to various files for compliance. (#217)

--- src/usecase/site/SiteController.php
+++ src/usecase/site/SiteController.php
@@ -7,8 +7,17 @@ namespace app\usecase\site;
 use app\usecase\Controller;
 use yii\web\ErrorAction;
 
+/**
+ * Site web controller.
+ *
+ * @copyright Copyright (C) 2025 Terabytesoftw.
+ * @license https://opensource.org/license/bsd-3-clause BSD 3-Clause License.
+ */
 final class SiteController extends Controller
 {
+    /**
+     * Renders the home page.
+     */
     public function actionIndex(): string
     {
         return $this->render('index');
