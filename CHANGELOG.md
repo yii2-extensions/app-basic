b3806355a7c1e7fe2376b15a029fd1930cd47cb1
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2026-02-20 20:27:08 -0300

    build(deps): Update `yii2-extensions/road-runner` `^0.1` to `^0.2`. (#234)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@
 - Bug #223: Remove outdated composer asset dependencies from `package.json` (@terabytesoftw)
 - Enh #229: Add `php-forge/coding-standard` to development dependencies for code quality checks (@terabytesoftw)
 - Bug #230: Add section for automated refactoring using `Rector` in testing documentation (@terabytesoftw)
+- Dep #234: Update `yii2-extensions/road-runner` `^0.1` to `^0.2` (@terabytesoftw)
 
 ## 0.1.2 October 8, 2025
 
--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use yii2\extensions\psrbridge\http\StatelessApplication;
+use yii2\extensions\psrbridge\http\Application;
 use yii2\extensions\roadrunner\RoadRunner;
 
 define('YII_DEBUG', getenv('YII_DEBUG') ?? false);
@@ -14,6 +14,6 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require dirname(__DIR__) . '/config/web/app.php';
 
-$runner = new RoadRunner(new StatelessApplication($config));
+$runner = new RoadRunner(new Application($config));
 
 $runner->run();
